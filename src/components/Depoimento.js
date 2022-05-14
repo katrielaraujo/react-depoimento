084c0eb067f6ed6e94daea9207e0f7902ebcddff
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-05-14 20:08:18 -0300

    concerto de bug a leitura de imagens

--- src/components/Depoimento.js
+++ src/components/Depoimento.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import '../stylesheets/Depoimento.css';
 
-function Depoimento(){
+function Depoimento(props){
     return (
         <div className='container-depoimento'>
             <img 
             className='img-depoimento'
-            src={require('../images/depoimento-emma.png')}
+            src={require(`../images/depoimento-shawn1.png`)}
             alt='Foto da Emma'/>
 
             <div className='container-texto-depoimento'>
                 <p className='nome-depoimento'>
-                Emma Bostian en Suecia
+                {} de {} 
                 </p>
                 <p className='cargo-depoimento'>
-                Engenharia de Software no Spotify
+                {} n {} 
                 </p>
                 <p className='texto-depoimento'>
-                Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.
+                {}
                 </p>
             </div>
         </div>
